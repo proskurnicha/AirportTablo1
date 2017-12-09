905de2440153d4df8d657cb8b556c713caf7f2b9
Natalia Proskurnicha <proskurnicha@gmail.com>
2017-12-09 20:19:55 +0200

    Add main classes

---
+++ AirportTablo1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AirportTablo1.MvcApplication" Language="C#" %>
---
+++ AirportTablo1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirportTablo1.Startup))]
+namespace AirportTablo1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
